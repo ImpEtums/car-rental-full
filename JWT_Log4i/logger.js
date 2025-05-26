6a87e3f272385e2ed98e5e459965d8951458a378
tffang <145011335+tffang@users.noreply.github.com>
2025-05-26 11:17:59 +0800

    Delete JWT_Log4i directory

--- JWT_Log4i/logger.js
+++
@@ -1,7 +0,0 @@
-import log4js from 'log4js';
-
-export const errorHandler = (err, req, res, next) => {
-  const logger = log4js.getLogger('Error');
-  logger.error(`路径 ${req.path} 发生错误: ${err.message}`);
-  res.status(500).json({ message: '服务器内部错误' });
-};
